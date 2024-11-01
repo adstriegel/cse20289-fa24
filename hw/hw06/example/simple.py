fae0a0b77a10be7cc4e390087c054209467d7047
Aaron Striegel <aaron.striegel@gmail.com>
2024-11-01 14:56:07 -0400

    Deeper example on unittests with subprocess execution

--- hw/hw06/example/simple.py
+++ hw/hw06/example/simple.py
@@ -1,5 +1,20 @@
 # simple.py : Simple code for unit testing
 
+import argparse
+
 def calcAdd (a, b):
     return a + b
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Simple code for unit testing')
+    parser.add_argument('a', type=int, help='First number')
+    parser.add_argument('b', type=int, help='Second number')
+    parser.add_argument('--file', type=str, help='File to read')
+    args = parser.parse_args()
+
+    print(calcAdd(args.a, args.b))
+
+    if args.file:
+        with open(args.file, 'r') as f:
+            print(f.read())
+            
--- hw/hw06/example/tests/test_simple.py
+++ hw/hw06/example/tests/test_simple.py
@@ -1,6 +1,7 @@
 # test_simple.py : Unit test for simple.py
 
 import unittest
+import subprocess
 
 # If you run your unit test from the upper directory (one up), this import will work due
 # to the presence of the __init__.py file in the tests directory
@@ -11,7 +12,14 @@ from simple import calcAdd
 
 class TestSimple(unittest.TestCase):
     def test_sum(self):
-            self.assertEqual(calcAdd(5,5), 10, 'The sum is wrong.')
+        self.assertEqual(calcAdd(5,5), 10, 'The sum is wrong.')
+
+    def test_runok (self):
+        theResult = subprocess.run(['python3', 'simple.py', '5', '5'], capture_output=True, text=True)        
+        self.assertEqual(int(theResult.stdout), 10, 'Failed to output the correct computed result of 10')
+
+    def test_runfile (self):
+        theResult = subprocess.run(['python3', 'simple.py', '5', '5', '--file', 'tests/inputs/text.txt'], capture_output=True, text=True)        
 
 if __name__ == '__main__':
     print('Starting the unit test')
